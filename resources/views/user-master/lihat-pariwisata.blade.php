fd412ffc94c3b5ba0bf4fe50ad318012cbe90cb1
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2021-03-25 17:03:57 +0700

    Update lihat-pariwisata.blade.php

--- resources/views/user-master/lihat-pariwisata.blade.php
+++ resources/views/user-master/lihat-pariwisata.blade.php
@@ -29,7 +29,7 @@
                             <th scope="col">Judul</th>
                             <th scope="col">Desc</th>
                             <th scope="col">Alamat</th>
-                            <th scope="col">Tiket</th>
+                            <th scope="col">Harga Tiket Masuk</th>
                             <th scope="col">Contact</th>
                             <th scope="col">Gambar</th>
                             <th scope="col">Fasilitas</th>
@@ -44,6 +44,80 @@
                 </div>
               </div>
     </div>
+
+
+@endsection
+@section('script')
+<script>
+    $('.data-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('lihat.pariwisata')}}",
+        columns: [
+            {
+                data: 'DT_RowIndex',
+                name: 'DT_RowIndex',
+            },            
+            {
+                data: 'judul',
+                name: 'judul',
+            },
+            {
+                data: 'deskripsi',
+                name: 'deskripsi',
+            },
+            {
+                data: 'alamat',
+                name: 'alamat',
+            },
+            {
+                data: 'htm',
+                name: 'htm',
+            },
+            {
+                data: 'kontak',
+                name: 'kontak',
+            },
+            {
+                data: 'gambar',
+                name: 'gambar',
+                render: function(data) {
+                return '<img width="100px" src="/'+data+'">'
+            },
+            },
+            {
+                data: 'fasilitas',
+                name: 'fasilitas',
+            },
+            {
+                data: 'action',
+                name: 'action',
+            },
+        ]
+    });
+    
+</script>
+
+<script>
+  $('body').on('click','.delete-confirm',function (event) {
+    event.preventDefault();
+    const url = $(this).attr('href');
+
+    Swal.fire({
+      title: 'Apakah Kamu Yakin ? ',
+      text: "Hapus Data ini!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Ya, Hapus'
+    }).then((result) => {
+      if (result.value) {
+        window.location.href = url;
+      }
+    })
+  });
+</script>
 
 
 @endsection
